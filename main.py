125dd2ddeb44328b574a7f6a2ff0f7d74ceb916c
Grigory <grigory@clearscale.net>
2019-04-23 20:58:06 +0700

    first_commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+name = input("Как тебя зовут?")
+sleep(1)
+
+print("Напиши 'начать_игру'")
+run = False
+characters = []
+while not run:
+    command = input()
+    if command == "начать_игру":
+        run = True
+        print(name, ", приветствую тебя в 'паки с персонажами'! Держи начального персонажа - Шелли!")
+        sleep(1)
+        print("Вы получили нового персонажа - Шелли!")
+        characters.append("Шелли")
+        sleep(1)
+        print("Напиши 'команды'")
+        command = input()
+while True:
+    if command == "команды":
+        print(" персонажи \n бой \n открыть_пак \n открыть_большой_пак \n открыть")
+    else:
+        print("Я тебя не понял. Напиши 'команды', чтобы узнать возможные команды.")
+    command = input()
